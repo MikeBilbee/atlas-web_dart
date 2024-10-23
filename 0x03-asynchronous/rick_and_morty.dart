4168d4bd73ac1891e0f0a8d53ecea4225a24208d
MikeBilbee <cmbilbee@gmail.com>
2024-10-23 13:18:51 -0500

    5.0 Rick and Morty list

--- 0x03-asynchronous/rick_and_morty.dart
+++ 0x03-asynchronous/rick_and_morty.dart
@@ -1,1 +1,19 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
 
+Future<void> printRmCharacters() async {
+  try {
+    var url = Uri.parse('https://rickandmortyapi.com/api/character');
+    var response = await http.get(url);
+
+    Map<String, dynamic> data = jsonDecode(response.body);
+
+    List<dynamic> characters = data['results'];
+
+    for (var character in characters) {
+      print(character['name']);
+    }
+  } catch (e) {
+    print('error caught: $e');
+  }
+}
