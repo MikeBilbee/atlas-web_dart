b38c6d9204f4fc2f2a4e452b96ef176ef136167e
MikeBilbee <cmbilbee@gmail.com>
2024-10-23 12:55:15 -0500

    1.1 fix & 2.0 get user

--- 0x03-asynchronous/1-get_user_id.dart
+++ 0x03-asynchronous/1-get_user_id.dart
@@ -2,13 +2,10 @@ import '1-util.dart';
 import 'dart:convert';
 
 Future<String> getUserId() async {
-  // Call the fetchUserData function to get the user data
   String userData = await fetchUserData();
 
-  // Extract the user ID from the user data
   Map<String, dynamic> userDataMap = jsonDecode(userData);
   String userId = userDataMap['id'];
 
-  // Return the user ID
   return userId;
 }
