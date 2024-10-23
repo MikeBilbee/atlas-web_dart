a7817c0ab67affc28ee0083b83bb94847efa12de
MikeBilbee <cmbilbee@gmail.com>
2024-10-23 12:58:35 -0500

    3.0 Greeting

---
+++ 0x03-asynchronous/3-greet_user.dart
@@ -0,0 +1,27 @@
+import 'dart:convert';
+import '3-util.dart';
+
+Future<String> greetUser() async {
+  try {
+    String userData = await fetchUserData();
+    Map<String, dynamic> userDataMap = jsonDecode(userData);
+    String username = userDataMap['username'];
+    return 'Hello $username';
+  } catch (e) {
+    return 'error caught: $e';
+  }
+}
+
+Future<String> loginUser() async {
+  try {
+    bool isUser = await checkCredentials();
+    print('There is a user: $isUser');
+    if (isUser) {
+      return await greetUser();
+    } else {
+      return 'Wrong credentials';
+    }
+  } catch (e) {
+    return 'error caught: $e';
+  }
+}
